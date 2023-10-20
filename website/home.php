a77eed577bc57a1a5f36fca5f9f708680c693715
S1lvveer <115066481+S1lvveer@users.noreply.github.com>
2023-10-20 17:14:08 +0200

    I have no idea
    My XAMPP doesn't work and it doesn't read ANY form of css so I couldn't get much done, I'll just find stuff about the countries we have and get it into .txt

--- website/home.php
+++ website/home.php
@@ -112,15 +112,9 @@
                 <li>Kyoto</li>
             </ul>
             
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-            laboris nisi ut aliquip ex ea commodo consequat.
+            <p class="description">
+            Lorem ipsum dolor sit amet, consx efgsadfgsadfsgdfgga commodo consequat. Lorem ipsum dolor sit amet, consx efgsadfgsadfsgdfgga commodo consequat. Lorem ipsum dolor sit amet, consx efgsadfgsadfsgdfgga commodo consequat.
             </p>
-            <!--
-                place p at the end
-            -->
         </div>
         </section>
         
