bc628c3687a18727bd15fcfe735a9a950419ec62
balaboo123 <146384000+balaboo123@users.noreply.github.com>
2023-10-22 16:04:46 +0200

    Moved every header to a simple require
    - Whenever you want to create the header, do "require_once('components/header.php')"

--- website/components/header.php
+++ website/components/header.php
@@ -27,17 +27,55 @@
 
         <div class="logo-container">
             <img src="../assets/globe.svg" alt="logo" class="logo">
+
+            <?php 
+            if ($fileName == "home") {// Only show this on the home page, although it could be everywhere theoretically..
+            ?>
+            <!-- Spinning "Globasia" around the logo [hide in editor]-->
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">>
+                <!-- Create text 1 & 2 on the outside -->
+                <text>
+                    <textPath href="#circle-path">Geoglasia</textPath>
+                    <animateTransform
+                    attributeName="transform"
+                    attributeType="XML"
+                    type="rotate"
+                    from="0 100 100"
+                    to="360 100 100"
+                    dur="20s"
+                    repeatCount="indefinite"
+                    />
+                </text>
+
+                <text>
+                    <textPath href="#circle-path">Geoglasia</textPath>
+                    <animateTransform
+                    attributeName="transform"
+                    attributeType="XML"
+                    type="rotate"
+                    from="180 100 100"
+                    to="540 100 100"
+                    dur="20s"
+                    repeatCount="indefinite"
+                    />
+                </text>
+
+                <!-- Define the path for the circle on the outside -->
+                <path id="circle-path" d="M 100,180 A 80,80 0 0,1 20,100" fill="transparent" />
+            </svg>
+
+            <?php } ?>
         </div>
         
 
         <div class="nav-links">
             <?php
-            // Display login when not logged in, display username & button to user settings if you are
+            // Display login when not logged in,
+            // else display stuff related to the user's account
 
             if ($user) {
                 $username = $user['login'];
-                echo 
-                "<h3>
+                echo "<h3>
                 <ion-icon name='person-circle-outline' class='icon'></ion-icon>
                 > Howdy, <span class='greeting'>$username!</span>
                 </h3>";
@@ -53,10 +91,16 @@
                 </a>";
             } else { 
             ?>
-                <a href="login.php" class="logintext">
-                    <ion-icon name="person-circle-outline" class="icon"></ion-icon>
-                    Login
-                </a>
+
+            <?php
+
+                // For anything BUT the login page - Login
+                if ($fileName != "login") 
+                    echo "<a href='login.php' class='logintext'>
+                        <ion-icon name='person-circle-outline' class='icon'></ion-icon>
+                        Login
+                    </a>";
+            ?>
             <?php 
             } 
             ?>
