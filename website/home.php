36b1b3f68cbe5d9f4a80089d5c210dbf29737b85
S1lvveer <115066481+S1lvveer@users.noreply.github.com>
2023-10-22 19:23:30 +0200

    updated timed swipe with keyboard control

--- website/home.php
+++ website/home.php
@@ -69,8 +69,10 @@
     // If you want to display data from the "places" table as well, use a similar approach with the $placesQuery.
 ?>
         </div>
-        </div>
+        <br><br>
         <div class="swiper-pagination"></div>
+        </div>
+        
         
     </main>
 
@@ -85,26 +87,23 @@
 <!-- Initialize Swiper -->
 <script>
   var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 10,
+    spaceBetween: 50,
+    centeredSlides: true,
     loop: true,
+    keyboard: {
+        enabled: true,
+      },
+    autoplay: {
+      delay: 6500,
+      disableOnInteraction: false,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
-    breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      768: {
-        slidesPerView: 4,
-        spaceBetween: 40,
-      },
-      1024: {
-        slidesPerView: 5,
-        spaceBetween: 50,
-      },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
   });
 </script>
